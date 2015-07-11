c1be263d855af321a42d0d58c5c958d546b93bae
Sean Baggaley <coffeescripter@gmail.com>
2015-07-11 17:02:42 +0100

    Various updates relating to the build script.

--- build.bat
+++ build.bat
@@ -3,11 +3,34 @@ for /F "usebackq tokens=1,2 delims==" %%i in (`wmic os get LocalDateTime /VALUE 
 SET no=%ldt:~2,6%
 SET start=%cd%
 
+IF /I "%PROCESSOR_ARCHITECTURE%"=="x32" (
+    IF NOT DEFINED PROCESSOR_ARCHITEW6432 (
+        SET ARCH=32
+    ) ELSE (
+        SET ARCH=64
+    )
+) ELSE IF /I "%PROCESSOR_ARCHITECTURE%"=="x86" (
+        IF NOT DEFINED PROCESSOR_ARCHITEW6432 (
+            SET ARCH=32
+        ) ELSE (
+            SET ARCH=64
+        )
+) ELSE (
+        SET ARCH=64
+)
+
+IF /I "%1"=="/FORCEWIN32" (
+    SET ARCH=32
+)
+
 echo All Out War "Theta" Build Script
-echo Written by AOSP
+echo Written by Sean
 echo.
 echo The build number is %no%.
 echo.
+
+SET PATH=%PATH%;%start%\utilties;%start%\utilties\x%ARCH%;%start%\utilities\acc
+
 echo Compiling ACS.
 
 if not exist src\code\acs\ (
@@ -15,11 +38,11 @@ if not exist src\code\acs\ (
 )
 
 cd src\code\acs_src
-"%start%\utilities\acschangelog.exe" "%start%\changelog.txt" "%start%\src\code\acs_src\a_changelog.acs"
+acschangelog "%start%\changelog.txt" "%start%\src\code\acs_src\a_changelog.acs"
 :: %start%\utilities\acver "a_version.acs" "%start%"
 
 cd "%start%"
-"%start%\utilities\acc\acc" "%start%\src\code\acs_src\aow2scrp.acs" "%start%\src\code\acs\aow2scrp.o"
+acc "%start%\src\code\acs_src\aow2scrp.acs" "%start%\src\code\acs\aow2scrp.o"
 
 if not %ERRORLEVEL%==0 (
     SET ACSError=%ERRORLEVEL%
@@ -31,7 +54,9 @@ if not %ERRORLEVEL%==0 (
     pause >nul
     exit %ACSError%
 )
-"%start%\utilities\acsconstants.exe" "%start%\src\code\acs_src\aow2scrp.acs" "%start%\src\code\actors\acsconstants.dec"
+
+acsconstants.exe "%start%\src\code\acs_src\aow2scrp.acs" "%start%\src\code\actors\acsconstants.dec"
+
 if not exist out\ (
 	mkdir out >nul
 ) else (
@@ -42,20 +67,24 @@ if not exist out\ (
 
 echo Packaging base...
 cd src\base\
-"%start%\utilities\7za" a -tzip "%start%\out\theta_base-%no%.pk3" *.* -r -xr!*.dbs -xr!*.backup1 -xr!*.backup2 -xr!*.backup3 -xr!*.bak
+7za a -tzip "%start%\out\theta_base-%no%.pk3" *.* -r -xr!*.dbs -xr!*.backup1 -xr!*.backup2 -xr!*.backup3 -xr!*.bak
 
 echo Packaging code...
 cd ..\..\
 cd src\code\
-"%start%\utilities\7za" a -tzip "%start%\out\theta_code-%no%.pk3" *.* -r -xr!*.dbs -xr!*.backup1 -xr!*.backup2 -xr!*.backup3 -xr!*.bak
+7za a -tzip "%start%\out\theta_code-%no%.pk3" *.* -r -xr!*.dbs -xr!*.backup1 -xr!*.backup2 -xr!*.backup3 -xr!*.bak
 
 echo Packaging maps...
 cd ..\..\
 cd src\maps\
-"%start%\utilities\7za" a -tzip "%start%\out\theta_maps-%no%.pk3" *.* -r -xr!*.dbs -xr!*.backup1 -xr!*.backup2 -xr!*.backup3 -xr!*.bak
+7za a -tzip "%start%\out\theta_maps-%no%.pk3" *.* -r -xr!*.dbs -xr!*.backup1 -xr!*.backup2 -xr!*.backup3 -xr!*.bak
 
 echo Packaging music...
 cd ..\..\
 cd src\music\
-"%start%\utilities\7za" a -tzip "%start%\out\theta_music-%no%.pk3" *.* -r -xr!*.dbs -xr!*.backup1 -xr!*.backup2 -xr!*.backup3 -xr!*.bak
+7za a -tzip "%start%\out\theta_music-%no%.pk3" *.* -r -xr!*.dbs -xr!*.backup1 -xr!*.backup2 -xr!*.backup3 -xr!*.bak
+
+echo The PK3 files are to be found in:
+echo %start%\out
+
 pause

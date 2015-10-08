65695770555dc865241f12f6cd42e9c7b5a01e36
Sean Baggaley <coffeescripter@gmail.com>
2015-10-08 20:08:32 +0100

    Update build scripts.

--- utilities/linux/wmktheta.sh
+++ utilities/linux/wmktheta.sh
@@ -17,16 +17,6 @@ else
 	typeset git=true
 fi
 
-if [[ "$1" == "--no-version" || "$1" == "-n" || "$2" == "--no-version" || "$2" == "-n" ]]; then
-	typeset no=""
-else
-	if [[ git == true ]]; then
-		typeset no="-$(date +\"%y%m%d\")-$(git log -1 --format=%h)"
-	else
-		typeset no="-$(date +\"%y%m%d\")"
-	fi
-fi
-
 if [[ "$1" == "--jenkins" ]]; then
 	typeset git=true
 	typeset no="-$(date +\"%y%m%d-%H%M\")-$(git log -1 --format=%h)"
@@ -70,6 +60,20 @@ fi
 
 x "utilities/linux/gitcommit c src/code/acs_src/gitcommit.acs --silent" "Making gitcommit.acs"
 x "utilities/linux/genver c src/code/acs_src/version.acs --silent" "Making version.acs"
+x "utilities/linux/genver bash utilities/version.sh --silent"
+eval "$(cat utilities/version.h)"
+
+if [[ "$1" == "--no-version" || "$1" == "-n" || "$2" == "--no-version" || "$2" == "-n" ]]; then
+	typeset no=""
+else
+	if [[ git == true ]]; then
+		typeset no="-${VERSION_STRING}-$(git log -1 --format=%h)"
+	else
+		typeset no="-${VERSION_STRING}"
+	fi
+fi
+
+
 x "mkdir -p src/code/acs" "Making acs output folder"
 x "utilities/linux/acc src/code/acs_src/aow2scrp.acs src/code/acs/aow2scrp.o" "Compiling ACS ${reset}"
 x "utilities/linux/acsconstants src/code/acs_src/aow2scrp.acs src/code/actors/acsconstants.dec ${s}" "Generating ACS -> Decorate constants"
